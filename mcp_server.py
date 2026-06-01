d2238340a8bac28def5887c3621d3e13490de29d
jayaSinha <jaya.sinha00@gmail.com>
2026-06-01 14:54:22 +0530

    Initial Commit

---
+++ mcp_server.py
@@ -0,0 +1,50 @@
+from mcp.server.fastmcp import FastMCP
+from pydantic import Field
+
+mcp = FastMCP("DocumentMCP", log_level="ERROR")
+
+
+docs = {
+    "deposition.md": "This deposition covers the testimony of Angela Smith, P.E.",
+    "report.pdf": "The report details the state of a 20m condenser tower.",
+    "financials.docx": "These financials outline the project's budget and expenditures.",
+    "outlook.pdf": "This document presents the projected future performance of the system.",
+    "plan.md": "The plan outlines the steps for the project's implementation.",
+    "spec.txt": "These specifications define the technical requirements for the equipment.",
+}
+
+# TODO: Write a tool to read a doc
+@mcp.tool(
+    name="read_doc_contents",
+    description="Read the contents of documents and return it as a string."
+)
+def read_document(
+        doc_id: str = Field(description="ID of the document to read"),
+):
+    if doc_id not in docs:
+        raise ValueError(f"Document with id {doc_id} does not exist")
+    return docs[doc_id]
+
+# TODO: Write a tool to edit a doc
+@mcp.tool(
+    name="edit_document",
+    description="Edit a document by replacing a string in the document content with a new string"
+)
+def edit_document(
+        doc_id: str = Field(description="ID of the document to edit"),
+        old_str: str = Field(description="The text to replace. Must match exactly, including whitespace"),
+        new_str: str = Field(description="New text to insert8 in the document content"),
+):
+    if doc_id not in docs:
+        raise ValueError(f"Document with id {doc_id} does not exist")
+
+    docs[doc_id] = docs[doc_id].replace(old_str, new_str)
+
+# TODO: Write a resource to return all doc id's
+# TODO: Write a resource to return the contents of a particular doc
+# TODO: Write a prompt to rewrite a doc in markdown format
+# TODO: Write a prompt to summarize a doc
+
+
+if __name__ == "__main__":
+    mcp.run(transport="stdio")
